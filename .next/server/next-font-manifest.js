145ad1a308452431a3ee180bbdffc8abc56c371d
Vlad Danilov <vi.vladdani@gmail.com>
2025-04-16 21:43:41 +0800

    chore: Stop tracking .next build directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/vlad/Documents/AccountantApp/app/layout\":[\"static/media/569ce4b8f30dc480-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
